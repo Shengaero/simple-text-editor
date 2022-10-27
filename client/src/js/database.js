397b2e496f334060c2138950a62afd0006763697
Kaidan Gustave <kaidangustave@yahoo.com>
2022-10-27 17:27:11 -0500

    implemented idb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,10 @@
 import { openDB } from 'idb';
 
+const dbName = 'jate';
+const version = 1;
+
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB(dbName, version, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -12,10 +15,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const obj = { content };
+  const db = await openDB(dbName, version);
+  try {
+    await db.put('jate', obj);
+  } catch(err) {
+    console.log(err);
+  } finally {
+    db.close();
+  }
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB(dbName, version);
+  let content;
+  try {
+    content = (await db.getAll('jate'))[0].content;
+  } finally {
+    db.close();
+  }
+  return content;
+}
 
 initdb();
